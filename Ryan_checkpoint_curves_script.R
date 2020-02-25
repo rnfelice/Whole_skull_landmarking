0a42899066f44e1688f2066cff663b05b500b7e5
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2020-02-25 14:20:21 +0000

    Create Ryan_checkpoint_curves_script.R

---
+++ Ryan_checkpoint_curves_script.R
@@ -0,0 +1,121 @@
+
+
+#rm(list=ls())
+library(tidyverse)
+library(Morpho)
+library(geomorph)
+library(Rvcg)
+library(paleomorph)
+library(EMMLi)
+library(qgraph)
+library(ape)
+library(geiger)
+library(abind)
+#load the script for resampling functions
+source("C:/Users/carlb/Box Sync/Carla Bardua/code from lab/resamplingsV3.R")
+#set the working directory
+setwd("C:/Users/carlb/Box Sync/Carla Bardua/Temnospondyls/Testing_Checkpoint_27_01/pts")
+​
+#curvedata must be a csv with columns 'lm1' 'lm2' and 'ptswanted'
+curvedata<-read_csv("C:/Users/carlb/Box Sync/Carla Bardua/Temnospondyls/Testing_Checkpoint_27_01/new curves_temnos_test.csv")
+#list which landmarks are fixed and will not slide
+fixed <- c(1:3)
+#load in the list of pts files from the current working directory
+ptslist <- dir(pattern='.pts', recursive=F)  
+pts_tibble <- as_tibble()#initialize an empty tibble to store the pts data
+filenames <- ptslist
+ntaxa <- length(filenames) #define the number of taxa based on how many pts files are in the working directory
+​
+#define the curves using the 'curvedata' object
+subsampled.curve<-sapply(paste("SC",c(1:nrow(curvedata)),sep=""),function(x) NULL)
+subsampled.curve[[1]]<-c(curvedata$lm1[1],((length(fixed)+1):(length(fixed)+curvedata$ptswanted[1])),curvedata$lm2[1])
+for (i in 2:length(subsampled.curve)){
+  subsampled.curve[[i]]<-c(curvedata$lm1[i],((max(unlist(subsampled.curve))+1):(max(unlist(subsampled.curve))+curvedata$ptswanted[i])),curvedata$lm2[i])
+}
+​
+subsampled.curve.in<-subsampled.curve
+for(i in 1:length(subsampled.curve.in)){
+  subsampled.curve.in[[i]]<-tail(head(subsampled.curve[[i]],-1),-1)
+}
+​
+​
+slidings.sub<-c((max(fixed)+1):max(unlist(subsampled.curve)))
+​
+## IMPORT DATA ##
+{
+  for(i in 1:length(ptslist))
+  {
+    specimen.tmp <- as_tibble(read.table(file=ptslist[i],skip=2,header=F,sep="")) #import a single specimen
+    specimen.tmp <- specimen.tmp %>% mutate(., V1 = as.character(V1)) #convert the first row, the lm names, to characters instead of factors
+    specimen.tmp <- specimen.tmp %>% mutate(.,spec.id=filenames[i]) #add a column with the specimen name
+    pts_tibble<-bind_rows(pts_tibble,specimen.tmp) #paste it to the end of the tibble with the rest of the specimens 
+  }
+  #this will give a warning message but its nothing to worry about
+  pts_tibble <- pts_tibble %>% separate(., V1, into = c("class", "id","sub_lm"), remove = FALSE)
+  
+  pts_tibble <- pts_tibble %>% mutate(., id = as.factor(id)) %>%
+    rename(., index = V1, X = V2, Y = V3, Z = V4) #rename the coordinate data columns
+  print("SERIOUSLY DONT WORRY ABOUT THE WARNINGS BELOW")
+}
+​
+#make a list of how many sliding semilandmarks on curves there are
+curvepoints <- sum(curvedata$ptswanted)
+#convert the tibble to a 3D array compatable with geomorph
+pts_tibble_tmp <- pts_tibble%>%filter(.,class=="S")%>%group_by(spec.id)%>%select(.,X,Y,Z)%>%nest()%>%transpose()
+ptsarray_tmp <- array(dim=c(length(fixed),3,ntaxa))
+​
+for(i in 1:length(pts_tibble_tmp))
+{
+  ptsarray_tmp[,,i] <- as.matrix(select(pts_tibble_tmp[[i]]$data, c(X,Y,Z)))
+}
+​
+​
+​
+##############################################################
+####making all the curves have the correct number of landmarks
+##############################################################
+#make an empty array with the correct number of landmarks and specimens 
+newpts <- array(data = NA, dim = c(length(fixed),3,ntaxa))
+#give it dimension names based on your specimen list
+dimnames(newpts)[3] <- list(substr(filenames,1,(nchar(filenames)-4)))
+#fill in the fixed landmarks 
+newpts[fixed,,] <- ptsarray_tmp
+​
+#OPTIONAL:
+#check for outliers in your anatomical landmarks before subsampling
+#find.outliers(newpts)
+​
+​
+​
+for (which.curve in 1:nrow(curvedata)){
+  this.curve <- array(data=NA, dim=c(curvedata$ptswanted[which.curve],3,ntaxa))
+  for (which.spec in 1:length(filenames)){
+    orig.curve <- pts_tibble %>% filter(.,spec.id==filenames[which.spec])%>%filter(., class=="C")%>%filter(., id==which.curve) %>% select(., X,Y,Z)
+    orig.curve.anchors <- pts_tibble %>% filter(.,spec.id==filenames[which.spec])%>%slice(c(subsampled.curve[[which.curve]][1],last(subsampled.curve[[which.curve]]))) %>% select(., X,Y,Z)
+    orig.curve <- rbind(orig.curve.anchors[1,],orig.curve,orig.curve.anchors[2,])
+    new.curve <- cursub.interpo(orig.curve, curvedata$ptswanted[which.curve])
+    this.curve[,,which.spec] <- as.matrix(new.curve)[2:(dim(new.curve)[1]-1),]
+  }
+  newpts <- abind::abind(newpts, this.curve, along=1)
+}
+​
+which.curve
+​
+class(as.integer(sprintf('%0.3d', 1:480)))
+class(which.curve)
+​
+which.curve2=sprintf('%0.3d', 1:480)
+which.curve2[1]
+​
+### trial for one spec one curve- can ignore ### 
+which.curve=2
+this.curve <- array(data=NA, dim=c(curvedata$ptswanted[which.curve],3,ntaxa))
+​
+which.spec=1
+​
+orig.curve <- pts_tibble %>% filter(.,spec.id==filenames[which.spec])%>%filter(., class=="C")%>%filter(., id==which.curve) %>% select(., X,Y,Z)
+orig.curve.anchors <- pts_tibble %>% filter(.,spec.id==filenames[which.spec])%>%slice(c(subsampled.curve[[which.curve]][1],last(subsampled.curve[[which.curve]]))) %>% select(., X,Y,Z)
+orig.curve <- rbind(orig.curve.anchors[1,],orig.curve,orig.curve.anchors[2,])
+new.curve <- cursub.interpo(orig.curve, curvedata$ptswanted[which.curve])
+this.curve[,,which.spec] <- as.matrix(new.curve)[2:(dim(new.curve)[1]-1),]
+newpts <- abind::abind(newpts, this.curve, along=1)
