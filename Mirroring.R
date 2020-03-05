ef90199b39de6cc39430f510350c4fcf460358f3
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2020-03-05 12:03:53 +0000

    Update Mirroring.R

--- Mirroring.R
+++ Mirroring.R
@@ -10,49 +10,56 @@
 #Ellen's code 
 #DATA SHOULD NOT BE PROCRUSTED 
 
+###################################################################################################
+##########################
 
-#Remove landmarks from the RHS (so that we can mirror the whole set of curves and landmarks)
-newptsnoRHS <- newnewpts[-c(67:123),,]
+#Data comes from read in and estimate.missing added data 
+
+library(paleomorph)
+
+
+
+#Remove the RHS lms if using the ptsbatch data 
+mirrordata <- ptsbatch[-c(67:123),,]
 #This allows us to see the numbered landmarks 
-text3d(newptsnoRHS[,,3], text=1:dim(newptsnoRHS)[1])
+text3d(ptsbatch[,,3], text=1:dim(mirrordata)[1])
 
 
-#Set the midline 
-midline<-as.integer(c(38,40,48,49,51,54,55,56,61)) # LM that are on the midline + parasphenoid curve points + NO patch point
-#got length(midline)= 9 points on the midline
+#Define left lm 
+left.lm <- c(1:37,39,41:47,50,52,53,57:60,62:66) 
+midline<-as.integer(c(38,40,48,49,51,54,55,56,61))
+#Define fixed left hand curvbes 
+left.curves<-c(2:20, 22:34, 36:44, 46:47, 49:51, 53:55, 57:63) 
+left.curve.list<-unlist(subsampled.curve.in[left.curves])  ##subsampled.curve.in from T1
+leftside<-c(left.lm,left.curve.list) # RHS LMs+ RHS curves+all patch points
+num.missing<-(length(leftside)) # number of LMs to create= total RHS-current LHS LMs
 
-left.lm <- c(1:37,39,41:47,50,52,53,57:60,62:66,124:536) 
-#left.lm <- c(2,3,5:18,21:37,39,41:47,50,52,53,57:60,62:66)
-#exclude midline points. Last number = last number of newpts 
+blanks<-c((dim(mirrordata)[1]+1):(dim(mirrordata)[1]+num.missing)) 
+# to fill in blanks from one row past the last current point, for the number of rows needed (num.missing)
+rightside<-c(blanks)
+
+add_col_or_row = function(x, n = 1, add_col = T, fill = 0)
+{
+  m1 = matrix(x, ncol = if(add_col) nrow(x) * ncol(x) else nrow(x), byrow = T)
+  m2 = matrix(fill, nrow = if(add_col) dim(x)[3] else prod(dim(x)[-1]),
+              ncol = if(add_col) nrow(x) * n else n)
+  array(t(cbind(m1, m2)),
+        c(nrow(x) + ((!add_col) * n), ncol(x) + (add_col * n), dim(x)[3]))
+}
+
+specimens2<-add_col_or_row(mirrordata,n=num.missing,add_col=FALSE,fill=NA)
+dimnames(specimens2)[3]<-dimnames(mirrordata)[3]
+
+#Bind the left and right side together 
+bilats<-cbind(leftside, rightside)
+#This has all the mirrored data 
+Mirrored_data<-mirrorfill(specimens2,l1=midline,l2=bilats)
+dimnames(Mirrored_data)[3]<-dimnames(mirrordata)[3]
 
 
-lengmatrice=dim(newptsnoRHS)[1]*2-length(midline)#-length(nasalfrontal) #should be the length with the both sides, 1 is the column and 2 
-#just means that we are duplicating the data to be on both sides of the skull 
+spheres3d(Mirrored_data[,,45])
 
+col=rainbow(length(1:dim(Mirrored_data)[1]))
+shapes3d(Mirrored_data[,,45], joinline=c(1:dim(Mirrored_data)[1]), color=col)
 
-Matrice=array(NA,dim = c(lengmatrice,3,188)) #3 is the dimensions (x, y, z), 2 is specimen number 
-Matrice[1:dim(newptsnoRHS)[1],,]=newptsnoRHS
-
-
-#left.lm <- c(1:37,39,41:47,50,52,53,57:60,62:66)
-#left.lm <- c(2,3,5:18,21:37,39,41:47,50,52,53,57:60,62:66)
-#exclude midline points. Last number = last number of newpts 
-
-#Check left.lm and midline [left.lm,,x] = species number
-spheres3d(newptsnoRHS[left.lm,,36],radius=2.5)
-spheres3d(newptsnoRHS[midline,,36],radius=3,col='red')
-
-right.lm <- c(67:123,537:1063) #left.lm +1:lenmatrice
-
-bilat.landmarks <- cbind(left.lm, right.lm) #one column is the rows of the right side LM and the other column the rows of the left side
-
-Mirroredpts=mirrorfill(A=Matrice,  l1=midline, l2=bilat.landmarks) # the NA rows are now filled so that the numbers are the same on both
-#sides of the skull 
-Mirroredpts
-deformGrid3d(Mirroredpts[537:1063,,2], Matrice[,,2], ngrid=0) #This shows you the new mirroed landmarks 
-
-#can't do with Procrusted data
-#This shows the original landmarks
-spheres3d(Mirroredpts[c(1:67),,67],col=2,radius=3)
-
-spheres3d(Mirroredpts[,,1],col=2,radius=3)
+save(Mirrored_data, file="F:/PTS FINAL LHS.R")
