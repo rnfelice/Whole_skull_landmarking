4cfaed6c69410d6ddd5b517ff0c5f51b57d6914b
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2020-04-20 21:17:35 +0100

    Update Mirroring_odontocetes.R

--- Mirroring_odontocetes.R
+++ Mirroring_odontocetes.R
@@ -1,4 +1,7 @@
 
+#Mirroring in the odontocetes 
+#Delaing with asymmetry
+
 midline.lm<-as.integer(c(38,40,48,49,51,54,55,56,61)) #midline landmarks 
 midline.curves <- ptsbatch[c(629:648,764:783,824:838,884:903),,] #midline curves 
 lmextra <- as.integer(c(67:79,83:86,120:123)) #left hand landmarks 
@@ -19,10 +22,9 @@ spheres3d(ptsbatch[corressLHS,,3],radius=2, color ='green')
 spheres3d(corresscurves[,,3],radius=2, color = 'green') 
 
 
-
-lm_to_mirror <- ptsbatch[-c(1:15,19:24,67:79,83:86,120:123, 124:1408),,] #take away the extra lms and curves 
+lm_to_mirror <- ptsbatch[-c(1:15,19:24,67:79,83:86,120:123,124:1408,38,40,48,49,51,54,55,56,61),,] #take away the extra lms and curves 
 #lm_to_mirror2 <- lm_to_mirror[-c(corressLHS),,] #this takes away the corresponding lms on the LHS
-curves_to_mirror <- ptsbatch[-c(1:123, 124:448, 1084:1408),,] #take away the extra curves and landmarks (corresponding curves) and the odont curves
+curves_to_mirror <- ptsbatch[-c(1:123, 124:448,1084:1408,629:648,764:783,824:838,884:903),,] #take away the extra curves and landmarks (corresponding curves) and the odont curves
 
 
 #check this is correct 
@@ -34,7 +36,9 @@ spheres3d(curves_to_mirror[,,3],radius=2, color ='blue')
 
 to_mirror <- abind(lm_to_mirror, curves_to_mirror, along = 1)
 
+
 #check this looks right 
 spheres3d(to_mirror[,,3],radius=2, color ='blue')
 spheres3d(ptsbatch[midline.lm,,3],radius=2, color = 'red') #check the midline 
 spheres3d(midline.curves[,,3], radius = 2, color = 'red') 
+
