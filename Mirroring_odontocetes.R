58f81b5443370d07f45404db6d9aa5e4ad024a7a
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2020-04-28 13:09:50 +0100

    Update Mirroring_odontocetes.R

--- Mirroring_odontocetes.R
+++ Mirroring_odontocetes.R
@@ -8,6 +8,8 @@ library(rgl)
 #asymmetric specimens landmarks = 1:123 
 #asymmetric spcimens curves = 1:85 (lines 1084:1408)
 
+#LHS = original 
+#RHS = asymmetric 
 
 load(ptsbatch.R) #Just 4 specimens to play with. Used estimate.missing on one of them to test it works ok
 curvedata <- read.csv('regions_odontocete.csv') #this is what I used to number the curves e.g. for mirroring and for the midline etc 
@@ -22,19 +24,19 @@ lmextra <- as.integer(c(67:79,83:86,120:123))
 curveextra <- ptsbatch[c(1084:1408),,] #1084 is curve #65 
 
 #Remove corresponding LHS landmarks and curves 
-corressLHS <- as.integer(c(1:15,19:24)) #LMs
-corresscurves <- ptsbatch[c(124:448),,] #curves 
+corressLHS <- as.integer(c(1:15,19:24)) #LMs -16-18 are the jugal which I'm ignoring
+corresscurves <- ptsbatch[c(124:448),,] #curves #i.e. curve 1-21 (inlcusive) 
                              
 #check the midline 
 spheres3d(ptsbatch[midline.lm,,3],radius=2, color = 'red') 
 #check the midline 
 spheres3d(midline.curves[,,3], radius = 2, color = 'red') 
-#check the additional LHS landmarks
+#check the additional RHS landmarks
 spheres3d(ptsbatch[lmextra,,3],radius=2) 
-#check the additional LHS curves 
+#check the additional RHS curves 
 spheres3d(curveextra[,,3],radius=2) 
 
-#Check corressponding landmarks and curves 
+#Check corressponding landmarks and curves on LHS (original) 
 spheres3d(ptsbatch[corressLHS,,3],radius=2, color ='green')
 spheres3d(corresscurves[,,3],radius=2, color = 'green') 
 
