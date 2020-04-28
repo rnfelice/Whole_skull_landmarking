89fa28e3742bcaa2e81f526931a6eefc0d37fe4e
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2020-04-28 15:16:22 +0100

    Update Mirroring_odontocetes.R

--- Mirroring_odontocetes.R
+++ Mirroring_odontocetes.R
@@ -16,34 +16,34 @@ ptsbatch <- load('ptsbatch.R') #Just 4 specimens to play with. Used estimate.mis
 curvedata <- read.csv('regions_odontocete.csv') #this is what I used to number the curves e.g. for mirroring and for the midline etc 
 
 #midline landmarks 
-midline.lm<-as.integer(c(38,40,48,49,51,54,55,56,61)) 
+midline.lm<-ptsbatch[c(38,40,48,49,51,54,55,56,61),,] 
 #midline curves 
 midline.curves <- ptsbatch[c(629:648,764:783,824:838,884:903),,] 
 #Extra landmarks placed on the RHS of odontocetes 
-lmextra <- as.integer(c(67:79,83:86,120:123)) 
+lmextra <- ptsbatch[c(67:79,83:86,120:123),,]
 #Extra curves placed on the RHS of odontocetes 
 curveextra <- ptsbatch[c(1084:1408),,] #1084 is curve #65 
 
 #Remove corresponding LHS landmarks and curves 
-corressLHS <- as.integer(c(1:15,19:24)) #LMs -16-18 are the jugal which I'm ignoring
+corressLHS <- ptsbatch[c(1:15,19:24),,] #LMs -16-18 are the jugal which I'm ignoring
 corresscurves <- ptsbatch[c(124:448),,] #curves #i.e. curve 1-21 (inlcusive) 
                              
 #check the midline 
-spheres3d(ptsbatch[midline.lm,,3],radius=2, color = 'red') 
+spheres3d(midline.lm[,,3],radius=2, color = 'red') 
 #check the midline 
 spheres3d(midline.curves[,,3], radius = 2, color = 'red') 
 #check the additional RHS landmarks
-spheres3d(ptsbatch[lmextra,,3],radius=2) 
+spheres3d(lmextra[,,3],radius=2) 
 #check the additional RHS curves 
 spheres3d(curveextra[,,3],radius=2) 
 
 #Check corressponding landmarks and curves on LHS (original) 
-spheres3d(ptsbatch[corressLHS,,3],radius=2, color ='green')
+spheres3d(corressLHS[,,3],radius=2, color ='green')
 spheres3d(corresscurves[,,3],radius=2, color = 'green') 
 
 #Not sure why I couldn't get 'corressLHS' to work so I just put in the LM numbers again...
 #take away the extra lms and curves 
-lm_to_mirror <- ptsbatch[-c(1:15,19:24,67:79,83:86,120:123,124:1408,38,40,48,49,51,54,55,56,61),,]
+lm_to_mirror <- ptsbatch[-c(1:15,19:24,67:123,83:86,120:123,124:1408,38,40,48,49,51,54,55,56,61),,]
 #lm_to_mirror2 <- lm_to_mirror[-c(corressLHS),,] #this takes away the corresponding lms on the LHS - doesn't work so have put the numbers in
 #take away the extra curves and landmarks (corresponding curves) and the odont curves
 curves_to_mirror <- ptsbatch[-c(1:123, 124:448,1084:1408,629:648,764:783,824:838,884:903),,] 
